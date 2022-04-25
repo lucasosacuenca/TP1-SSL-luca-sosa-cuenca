d7fd19b4504089128efc1527a33120c386a234a6
lucasosacuenca <lucasosacuenca@gmail.com>
2022-04-25 18:50:38 -0300

    TP1 Solucion

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    printf("Ingresar caractere: ");
+    char c;
+    while((c=getchar())!='\n')
+    {
+        if(islower(c))
+        {
+            printf("%c",toupper(c));
+        }
+        else if(isupper(c))
+        {
+            printf("%c",tolower(c));
+        }
+        else if(isdigit(c))
+        {            
+        }
+        else
+        {
+            printf("%c",c);
+        }        
+    }
+    return 0;
+}
